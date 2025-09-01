2d6f252afc4b1a3dc784e05057543a37aa1281e4
dndydon <don@sleeter.com>
2025-09-01 15:06:43 -0700

    Initial Commit

---
+++ TimeSince25/TimeSince25App.swift
@@ -0,0 +1,32 @@
+//
+//  TimeSince25App.swift
+//  TimeSince25
+//
+//  Created by Don Sleeter on 9/1/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TimeSince25App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TimeSince25Tests/TimeSince25Tests.swift
@@ -0,0 +1,16 @@
+//
+//  TimeSince25Tests.swift
+//  TimeSince25Tests
+//
+//  Created by Don Sleeter on 9/1/25.
+//
+
+import Testing
+
+struct TimeSince25Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
